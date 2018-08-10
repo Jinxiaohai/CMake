3c0804df0f3c8298ad88689122ee001bf4ce66fd
Jinxiaohai <xiaohaijin@outlook.com>
2018-08-10 23:33:29 +0800

    BasicStartingPoint

---
+++ BasicStartingPoint/build/CMakeFiles/BasicStartingPoint.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BasicStartingPoint.dir/src/tutorial.cpp.o"
+  "BasicStartingPoint.pdb"
+  "BasicStartingPoint"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BasicStartingPoint.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
